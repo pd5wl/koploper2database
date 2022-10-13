045c659b4891e9c1b9946aff26974796ac52a0d3
wijnandlaros <11857240+pd5wl@users.noreply.github.com>
2022-10-13 09:41:19 +0200

    File writing netter gemaakt

--- lib/filehandeling.py
+++ lib/filehandeling.py
@@ -10,12 +10,14 @@ now = datetime.datetime.now()
 
 if cfg.op_fileadd == 1:
     f = open(savefile, "a")
-    line1 = ("Aanmaak datum :", now.strftime("%Y-%m-%d %H:%M:%S"), '\n')
+    line1 = ("Aanmaak datum :", now.strftime("%Y-%m-%d %H:%M:%S"))
     kop1 = ','.join(str(v) for v in line1)
-    line2 = ("loc, blok, time, route", '\n')
+    line2 = "loc, blok, time, route"
     kop2 = ','.join(str(v) for v in line2)
     f.write(kop1)
+    f.write('\n')
     f.write(kop2)
+    f.write('\n')
     if cfg.dbg_print == 1:
         print(kop1)
         print(kop2)
@@ -24,12 +26,14 @@ if cfg.op_fileadd == 1:
     f.flush()
 else:
     f = open(savefile, "w")
-    line1 = ("Aanmaak datum :", now.strftime("%Y-%m-%d %H:%M:%S"), '\n')
+    line1 = ("Aanmaak datum :", now.strftime("%Y-%m-%d %H:%M:%S"))
     kop1 = ' '.join(str(v) for v in line1)
-    line2 = ("loc, blok, time, route", '\n')
+    line2 = "loc, blok, time, route"
     kop2 = ' '.join(str(v) for v in line2)
     f.write(kop1)
+    f.write('\n')
     f.write(kop2)
+    f.write('\n')
     if cfg.dbg_print == 1:
         print(kop1)
         print(kop2)
@@ -49,18 +53,20 @@ def passdata(datalist):
 
 # write with model time or real time
     if cfg.kl_tijd == 1:
-        line = (loc, blok, mod_time, route, '\n')
+        line = (loc, blok, mod_time, route)
         values = ','.join(str(v) for v in line)
         f.write(values)
+        f.write('\n')
         if cfg.dbg_print == 1:
             print(values)
         else:
             pass
         f.flush()
     else:
-        line = (loc, blok, real_time, route, '\n')
+        line = (loc, blok, real_time, route)
         values = ','.join(str(v) for v in line)
         f.write(values)
+        f.write('\n')
         if cfg.dbg_print == 1:
             print(values)
         else:
